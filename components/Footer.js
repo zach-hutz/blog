25b456c50d08797fc51bb28cc78956373cbed0fe
zhutton <zhutton@standarduserllc.com>
2022-07-30 12:40:50 -0500

    update

--- components/Footer.js
+++ components/Footer.js
@@ -22,9 +22,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          <Link href="https://github.com/zach-hutz/blog">Created with Next.JS</Link>
         </div>
       </div>
     </footer>
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
+  { href: 'https://zachhutton.com', title: 'Portfolio' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
