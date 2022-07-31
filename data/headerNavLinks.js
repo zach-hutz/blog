290be7ee9b559cbf856c2eb5cc3d7e8b4a31f5b4
zhutton <zhutton@standarduserllc.com>
2022-07-31 12:34:14 -0500

    remvoe page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: 'https://zachhutton.com', title: 'Portfolio' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
